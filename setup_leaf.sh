cc8f0402464b87b27162fd13ac132334e938bff5
icydoge <icydoge@gmail.com>
2020-04-12 03:40:02 +0100

    Use -mod vendor

--- setup_leaf.sh
+++ setup_leaf.sh
@@ -5,12 +5,11 @@ set -e
 GO_VERSION=1.14.2
 
 [ -f "/tmp/go$GO_VERSION.linux-amd64.tar.gz" ] || wget https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz -O /tmp/go1.14.2.linux-amd64.tar.gz
-[ -d "/usr/local/go" ] || tar -C /usr/local -zxf /tmp/go$GO_VERSION.linux-amd64.tar.gz
+[ -d "/usr/local/go" ] || sudo tar -C /usr/local -zxf /tmp/go$GO_VERSION.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 rm -rf /tmp/go$GO_VERSION.linux-amd64.tar.gz
 
 cd leaf
-go get -d -v
 sudo make install
 systemctl enable oxcross-leaf
 systemctl start oxcross-leaf
--- setup_origin.sh
+++ setup_origin.sh
@@ -5,12 +5,11 @@ set -e
 GO_VERSION=1.14.2
 
 [ -f "/tmp/go$GO_VERSION.linux-amd64.tar.gz" ] || wget https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz -O /tmp/go1.14.2.linux-amd64.tar.gz
-[ -d "/usr/local/go" ] || tar -C /usr/local -zxf /tmp/go$GO_VERSION.linux-amd64.tar.gz
+[ -d "/usr/local/go" ] || sudo tar -C /usr/local -zxf /tmp/go$GO_VERSION.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 rm -rf /tmp/go1.14.2.linux-amd64.tar.gz
 
 cd origin
-go get -d -v
 sudo make install
 systemctl enable oxcross-origin
 systemctl start oxcross-origin
