b36fa759749e6c91fd4230d87cc81e6a1d7afa2b
icydoge <icydoge@gmail.com>
2020-04-12 04:28:07 +0100

    Avoid redundant re-download

--- setup_leaf.sh
+++ setup_leaf.sh
@@ -12,7 +12,7 @@ GO_VERSION=1.14.2
 useradd oxcross || true
 sed -i 's/API_BASE_CHANGE_ME/$1/g' $(pwd)/leaf/oxcross-leaf.service 
 
-[ -f "/tmp/go$GO_VERSION.linux-amd64.tar.gz" ] || wget https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz -O /tmp/go1.14.2.linux-amd64.tar.gz
+[ -d "/usr/local/go" ] || [ -f "/tmp/go$GO_VERSION.linux-amd64.tar.gz" ] || wget https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz -O /tmp/go1.14.2.linux-amd64.tar.gz
 [ -d "/usr/local/go" ] || sudo tar -C /usr/local -zxf /tmp/go$GO_VERSION.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 rm -rf /tmp/go$GO_VERSION.linux-amd64.tar.gz
